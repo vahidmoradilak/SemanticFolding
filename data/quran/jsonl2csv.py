6a1aa7c6a1dc1a3dd566ee69509305e00e6a88ea
vahidmoradilak <vahidmoradilak@gmail.com>
2026-06-30 12:11:30 +0330

    Adding Quran file only in Arabic and only in English

--- data/quran/jsonl2csv.py
+++ data/quran/jsonl2csv.py
@@ -3,14 +3,18 @@ import csv
 
 # مسیر فایل JSONL دیتاست
 input_file = "multilingual_quran.jsonl"
-output_file = "quran_ayahs.csv"
+# output_file = "quran_ayahs.csv"
+# output_file = "quran_ayahs_ar.csv"
+output_file = "quran_ayahs_en.csv"
 
 with open(input_file, "r", encoding="utf-8") as f_in, \
      open(output_file, "w", encoding="utf-8", newline='') as f_out:
 
     writer = csv.writer(f_out)
     # نوشتن هدر (اختیاری)
-    writer.writerow(["line_number", "arabic_ayah", "english_translation"])
+    # writer.writerow(["line_number", "arabic_ayah", "english_translation"])
+    # writer.writerow(["line_number", "arabic_ayah"])
+    writer.writerow(["line_number", "english_translation"])
 
     for i, line in enumerate(f_in, 1):
         data = json.loads(line)
@@ -19,6 +23,8 @@ with open(input_file, "r", encoding="utf-8") as f_in, \
             # break
         arabic = data.get("verse_ar", "").replace("\n", " ").strip()
         english = data.get("verse_en", "").replace("\n", " ").strip()
-        writer.writerow([i, arabic, english])
+        # writer.writerow([i, arabic, english])
+        # writer.writerow([i, arabic])
+        writer.writerow([i, english])
 
 print(f"Saved {i} rows to {output_file}")
